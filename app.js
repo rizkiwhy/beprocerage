3b71461bc5b759ce04628eb82c40f5f7ddaa9fe3
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-07-27 09:11:18 +0700

    fix cross origin block

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.setHeader('Access-Control-Allow-Credentials', true)
     
     next()
