dcbcbc2ea67720680cfc149b24850a8ffc73a9b5
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-10-12 19:55:31 +0700

    fix cors origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const competencyunitRouter = require('./routes/competencyunit')
 const inboxRouter = require('./routes/inbox')
 const authRouter = require('./routes/auth')
 const path = require('path')
-const cors = require('cors')
+// const cors = require('cors')
 
 
 // const verifyFile = require('./middleware/verifyFile')
@@ -29,27 +29,27 @@ const cors = require('cors')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 // app.use(cors({ origin: ['http://103.148.113.86:8080'], }))
-const corsOptions ={
-    origin:'https://procerage.herokuapp.com/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'https://procerage.herokuapp.com/', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-    //     res.setHeader('Access-Control-Allow-Origin', '*')
-    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    //     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    //     res.setHeader('Access-Control-Allow-Credentials', true)
+app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+        res.setHeader('Access-Control-Allow-Credentials', true)
 
-    //     // res.setHeader('Access-Control-Allow-Origin', '*');
-//     // res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//     // res.setHeader("Access-Control-Max-Age", 3600);
-//     // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept, Authorization');
-//     // res.setHeader("Access-Control-Allow-Credentials", "true");
+        // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    // res.setHeader("Access-Control-Max-Age", 3600);
+    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept, Authorization');
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
 
-//     next()
-// })
+    next()
+})
 
 app.use('/api/v1', dessertRouter)
 app.use('/api/v1', [
